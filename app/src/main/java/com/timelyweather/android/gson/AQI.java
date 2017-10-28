7776d50ce312c4cbc826ff91a1fee7e762e11abc
gulingfengze <gulingfengze@126.com>
2017-10-28 12:45:51 +0800

    Update

---
+++ app/src/main/java/com/timelyweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.timelyweather.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
